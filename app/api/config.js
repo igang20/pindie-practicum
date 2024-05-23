503e8be58aaf3a037c209f1c013cad8cdc02bd70
Ilya <65218566+igang20@users.noreply.github.com>
2024-05-23 18:56:30 +0300

    .

--- app/api/config.js
+++ app/api/config.js
@@ -1,5 +1,6 @@
 // export const BASE_URL = "https://api-code-2.practicum-team.ru";
-export const BASE_URL = "http://localhost:3001/api";
+// export const BASE_URL = "http://localhost:3001/api";
+export const BASE_URL = "https://iluxauxa-backend.nomoredomainswork.ru/api";
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
